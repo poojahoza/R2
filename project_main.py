7caeada2083141dbe0b542b2e2619b5bf80c33e8
poojahoza <oza.pooja.h@gmail.com>
2021-06-22 11:10:55 -0400

    file change

--- project_main.py
+++ project_main.py
@@ -9,6 +9,6 @@ if __name__ == "__main__":
     # textgraph_read_write.textgraph_data('/media/poojaoza/extradrive1/datasets/tg2021-alldata-practice/data/tables/', 
     #                                     '/media/poojaoza/extradrive1/datasets/tg2021-alldata-practice/data/json_tables/', 
     #                                     filenames)
-    textgraph_read_write.process_textgraph_questions('/media/poojaoza/extradrive1/datasets/tg2021-alldata-practice/data/questions.train.tsv',
-                                                     '/media/poojaoza/extradrive1/datasets/tg2021-alldata-practice/data/json_questions/questions.train.json')
+    textgraph_read_write.process_textgraph_questions('/media/poojaoza/extradrive1/datasets/tg2021-alldata-practice/data/questions.dev.tsv',
+                                                     '/media/poojaoza/extradrive1/datasets/tg2021-alldata-practice/data/json_questions/questions.dev.json')
     
